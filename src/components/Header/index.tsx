b29156d852be66119298384c7a6c4870bd38f98b
Hugo Petla <hugopetla@gmail.com>
2023-11-30 22:57:39 -0300

    Creating layout to application

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <a href="">timer</a>
+        <a href="">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
